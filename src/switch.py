cf05c4c086526321def11693bbf91377427d1532
DiegoTavares <diego.exodo@gmail.com>
2017-07-09 17:42:46 -0300

    add class

--- src/switch.py
+++ src/switch.py
@@ -2,15 +2,19 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+class GpioSwitch(object):
+    control = 0
 
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        pass
 
-def my_callback(channel):
-    print("falling edge detected on 18")
+    def my_callback(self, channel):
+        print("falling edge detected on 18")
 
-GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime=300)
+    def start(self):
+        GPIO.add_event_detect(18, GPIO.FALLING, callback=self.my_callback, bouncetime=300)
 
-time.sleep(10)
-GPIO.cleanup()           # clean up GPIO on normal exit
+    def __del__(self):
+        GPIO.cleanup()
