8e36c2e93c003431f5348739e979ec9099562265
DiegoTavares <diego.exodo@gmail.com>
2017-07-09 19:22:12 -0300

    Add timer

--- src/switch.py
+++ src/switch.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO
-import time
+from datetime import datetime
 
 
 class GpioSwitch(object):
     control = 0
+    time_start = 0
+    COMMAND_TIMEOUT = 5
 
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(18, GPIO.IN,pull_up_down=GPIO.PUD_UP)
-        pass
+        GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    def my_callback(self, channel):
-        print("falling edge detected on 18")
+    def trigger_callback(self, channel):
+        # Check timeout
+        if (datetime.now() - self.time_start).total_seconds() > self.COMMAND_TIMEOUT:
+            self.control = 0
+
+        if self.control == 0:
+            self.time_start = datetime.now()
+        self.control += 1
+
+        if self.control == 1:
+            print('control', 1)
+        elif self.control == 2:
+            print('control', 2)
 
     def start(self):
-        GPIO.add_event_detect(18, GPIO.FALLING, callback=self.my_callback, bouncetime=300)
+        GPIO.add_event_detect(18, GPIO.FALLING, callback=self.trigger_callback, bouncetime=300)
 
     def __del__(self):
         GPIO.cleanup()
